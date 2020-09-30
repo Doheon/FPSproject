cff7fd0311d8489320b6ae67b0ca1915cb5fe964
Doheon <andh9696@gmail.com>
2020-09-30 16:54:41 +0900

    move

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : LivingEntity
+{
+    //hp, sp, skill, 공격력, 등등 모든 상태 조절
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<UIManager>();
+            }
+
+            return m_instance;
+        }
+    }
+    private static UIManager m_instance;
+
+    public Gun gun;
+    public GameObject go_BulletHUD;
+    
+    //총알 텍스트
+    public Text BulletText;
+    public GameObject theSniperMode;
+    public GameObject theCrossHair;
+
+    private void start(){
+        BulletText.text = gun.currentBullet + "/" + gun.maxBullet;
+        theSniperMode.SetActive(false);
+    }
+
+    public void updateBulletText(int _cur, int _max){
+        BulletText.text = _cur + "/" + _max;
+    }
+    public void SniperMode(){
+        theSniperMode.SetActive(true);
+        theCrossHair.SetActive(false);
+
+    }
+    public void cancelSniperMode(){
+        theSniperMode.SetActive(false);
+        theCrossHair.SetActive(true);
+    }
+}
